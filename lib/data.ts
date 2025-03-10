a110354a7c02e9dbcec9492eea64a497d0eabe36
misanthropi-codes <misanthropic.codes@outlook.com>
2025-03-10 16:20:57 +0530

    linkfix

--- lib/data.ts
+++ lib/data.ts
@@ -2,27 +2,33 @@ export const courses = [
   {
     id: 1,
     title: "Complete JEE Chemistry Crash Course",
-    description: "Master all chemistry concepts with detailed video lectures, solved problems, and mock practice. Includes advanced problem-solving techniques.",
+    description:
+      "Master all chemistry concepts with detailed video lectures, solved problems, and mock practice. Includes advanced problem-solving techniques.",
     price: "₹599",
-    image: "https://d502jbuhuh9wk.cloudfront.net/courses/6793a7ed6f8239249fa82f44/6793a7ed6f8239249fa82f44_scaled_cover.jpg?v=6",
-    link: "https://www.crackiit.live/courses/JEE-CHEMISTRY-CRASH-COURSE-6793a7ed6f8239249fa82f44"
+    image:
+      "https://d502jbuhuh9wk.cloudfront.net/courses/6793a7ed6f8239249fa82f44/6793a7ed6f8239249fa82f44_scaled_cover.jpg?v=6",
+    link: "https://learn.crackiit.site/courses/JEE-CHEMISTRY-CRASH-COURSE-6793a7ed6f8239249fa82f44",
   },
   {
     id: 2,
     title: "Complete College counselling",
-    description: "Empowering you with expert advice, practical tools, and personalized support to navigate college applications and conquer form filling stress!",
+    description:
+      "Empowering you with expert advice, practical tools, and personalized support to navigate college applications and conquer form filling stress!",
     price: "₹799",
-    image: "https://d502jbuhuh9wk.cloudfront.net/courses/6633328eba81d47e015b5ef5/6633328eba81d47e015b5ef5_scaled_cover.jpg?v=2",
-    link: "https://www.crackiit.live/courses/College-Counselling-All-in-one-6633328eba81d47e015b5ef5-6633328eba81d47e015b5ef5"
+    image:
+      "https://d502jbuhuh9wk.cloudfront.net/courses/6633328eba81d47e015b5ef5/6633328eba81d47e015b5ef5_scaled_cover.jpg?v=2",
+    link: "https://learn.crackiit.site/courses/College-Counselling-All-in-one-6633328eba81d47e015b5ef5-6633328eba81d47e015b5ef5",
   },
   {
     id: 3,
     title: "Jee Smashers Rocket M",
-    description: "Not Just a mentor But Your SAARTHI in your JEE Journey. Get the best mentorship and guidance to crack JEE 2026 with our expert faculty.",
+    description:
+      "Not Just a mentor But Your SAARTHI in your JEE Journey. Get the best mentorship and guidance to crack JEE 2026 with our expert faculty.",
     price: "₹699",
-    image: "https://d502jbuhuh9wk.cloudfront.net/courses/665491a436ab7a5775dd3f00/665491a436ab7a5775dd3f00_scaled_cover.jpg?v=4",
-    link: "https://www.crackiit.live/courses/JEE-Smashers-Annual-Udaan-Mentorship-JEE-2026-launchpad-665491a436ab7a5775dd3f00"
-  }
+    image:
+      "https://d502jbuhuh9wk.cloudfront.net/courses/665491a436ab7a5775dd3f00/665491a436ab7a5775dd3f00_scaled_cover.jpg?v=4",
+    link: "https://learn.crackiit.site/courses/JEE-Smashers-ROCKET-Launchpad-66546d5d71cfa96f47fb8a98",
+  },
 ];
 
 export const testimonials = [
@@ -30,27 +36,24 @@ export const testimonials = [
     id: 1,
     name: "Manav Chauhan",
     text: "I have scored 99.71%ile i would Thank CrackIIT: Jee smashers and Mjk sir for providing me the guidance and motivating me to study so that i could work hard for my goals. Thank you sir for Staying there for me till the end ",
-    image: "/"
+    image: "/",
   },
   {
     id: 2,
     name: "Priyanjan Maiti",
     text: "I have scored 99.756%ile Through these two years your videos have provided me with the much needed guidance to suceed in this exam . Without your knowledge and experience my preparation would have been incomplete ",
-    image: "/"
+    image: "/",
   },
   {
     id: 3,
     name: "Sheetal Dhote",
     text: " i have scored 98.6%ile and i would say that this the best online platform for JEE preparation! The quality of content and practice material is outstanding. Secured 99.8 percentile in JEE Mains.",
-   image: "/"
+    image: "/",
   },
   {
-    
-      id: 4,
-      name: "Rajat Kumar Singh",
-      text: "I have scored 99.3%ile and I am the Sikkim State Topper. Your videos have been an essential part of my preparation. The guidance and insights provided helped me stay focused and on track. Without your expertise, achieving this rank would have been difficult.",
-      image: "/"
-  
-  
+    id: 4,
+    name: "Rajat Kumar Singh",
+    text: "I have scored 99.3%ile and I am the Sikkim State Topper. Your videos have been an essential part of my preparation. The guidance and insights provided helped me stay focused and on track. Without your expertise, achieving this rank would have been difficult.",
+    image: "/",
   },
 ];
